f17a691485653d30a5aae4927b2f7f060057c746
marioalbornoz <mario.albornozc@utem.cl>
2021-02-23 21:07:55 -0300

    login de usuario: - verificar - validar estado del user - verificar contrasena

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,14 +1,56 @@
 const { request, response } = require('express');
+const Usuario = require('../models/usuario.db');
+const bcrypt = require('bcrypt');
 
-
-const login = (req = request, res = response) => {
+const login = async(req = request, res = response) => {
 
     const { correo, password } = req.body;
+
+    try {
+
+        // verificar si email existe
+        const usuario = await Usuario.findOne({ correo });
+        if( !usuario && !usuario.estado){
+            return res.status(400).json({
+                msg: 'Usuario o password incorrectos - correo'
+            })
+        }
+
+        // Verificar si el usuario esta activo
+        if(!usuario.estado){
+            return res.status(400).json({
+                msg: 'Usuario o password incorrectos - estado:false'
+            })
+        }
+
+
+
+        // Verificar la contrasena
+
+        const validPassword = bcrypt.compareSync( password, usuario.password);
+        if(!validPassword){
+            return res.status(400).json({
+                msg: 'Password incorrectos - Password incorrecta'
+            })
+        }
+
+
+        // Generar el JWT
+
     res.json({
         msg:'Login ok',
         correo,
         password
     })
+    
+} catch (error) {
+    console.log(error);
+    return res.status(500).json({
+        msg: 'Algo salio mal! Hable con el admin'
+    })
+    
+}
+
 }
 
 module.exports = {
