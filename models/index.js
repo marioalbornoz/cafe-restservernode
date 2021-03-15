b2bd2249b311217c48bb15d5abfe2fc4f4d21fc8
marioalbornoz <mario.albornozc@utem.cl>
2021-03-15 12:15:51 -0300

    Add model Categoria and index models on index.js

---
+++ models/index.js
@@ -0,0 +1,14 @@
+
+
+const Categoria = require('./categoria.db');
+const Rol = require('./rol.db');
+const Server = require('./server');
+const Usuario = require('./usuario.db');
+// const Categoria = require('./categoria.db');
+
+module.exports = {
+    Categoria,
+    Rol,
+    Server,
+    Usuario
+}
