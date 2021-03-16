be75483ff0b005a912aff3e39864c899b59fa6e4
marioalbornoz <mario.albornozc@utem.cl>
2021-03-16 09:46:32 -0300

    ruta y simple controller para realizar busqueda

--- models/server.js
+++ models/server.js
@@ -9,9 +9,10 @@ class Server {
 
         this.paths = {
             auth: '/api/auth',
+            buscar: '/api/buscar',
             categorias: '/api/categorias',
             usuarios: '/api/usuarios',
-            productos: '/api/productos'
+            productos: '/api/productos',
         }
         // this.usuariosPath = '/api/usuarios';
         // this.authPath ='/api/auth';
@@ -45,6 +46,7 @@ class Server {
 
     routes(){
         this.app.use(this.paths.auth, require('../routes/auth.routes') );
+        this.app.use(this.paths.buscar, require('../routes/buscar.routes') );
         this.app.use(this.paths.categorias, require('../routes/categorias.routes') );
         this.app.use(this.paths.productos, require('../routes/productos.routes') );
         this.app.use(this.paths.usuarios, require('../routes/user.routes') );
