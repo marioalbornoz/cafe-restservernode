098b1cf30ac4b9d282cbcdb6e9d3e084bfcc0af8
marioalbornoz <mario.albornozc@utem.cl>
2021-03-17 09:32:43 -0300

    validar extension

--- controller/uploads.controller.js
+++ controller/uploads.controller.js
@@ -10,17 +10,30 @@ const subirArchivos = (req, res = response) => {
     }
   
     const { archivo } = req.files;
+    const nombreCortado = archivo.name.split(".");
+    const extension = nombreCortado[nombreCortado.length -1 ];
+    console.log(extension);
+
+    // extensiones validadas
+    const extensionesvalidas = ['png','jpeg', 'jpg', 'gif'];
+    if(!extensionesvalidas.includes(extension)){
+        return res.status(400).json({
+            msg: `La ${extension} no es permitida, debe ser ${extensionesvalidas}`
+        })
+    }
+
+    res.json({extension})
   
-    const uploadPath = path.join(__dirname, '../uploads/',archivo.name);
+//     const uploadPath = path.join(__dirname, '../uploads/',archivo.name);
   
-    archivo.mv(uploadPath, (err) => {
-      if (err) {
-        return res.status(500).send(err);
-      }
+//     archivo.mv(uploadPath, (err) => {
+//       if (err) {
+//         return res.status(500).send(err);
+//       }
   
-      res.send('File uploaded to ' + uploadPath);
-    });
-}
+//       res.send('File uploaded to ' + uploadPath);
+//     });
+} 
 
 
 module.exports = {
